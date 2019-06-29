e265264bdf65dd4894ec1e2a7c041004d3901036
YuriiTrofimenko <tyaa@ukr.net>
2019-06-29 11:09:11 +0300

    init

---
+++ AnimalsLib/src/org/tyaa/animalslib/Cat.java
@@ -0,0 +1,11 @@
+package org.tyaa.animalslib;
+
+public class Cat extends AbstractPet implements ISoundable {
+
+    public void breathe() {
+        System.out.println("Дышу легкими");
+    }
+
+    public void sound() {
+    }
+}
